2a899a40ed288213acbca878f8fb70ce902e88f0
RizwanAli4574 <rizwanali4574992@gmail.com>
2026-01-12 12:28:45 +0500

    some practice Req Methods etc

--- NodeJs Practice/First Project/index.js
+++ NodeJs Practice/First Project/index.js
@@ -13,8 +13,21 @@ app.get('/', (req , res) => {
     res.jsonp({name: "Rizwan Ali"})
 })
 
-app.post('/about', (req , res) => {
-    res.send(req.body)
+// app.get('/about', (req , res) => {
+//   if(req.accepts('html')){
+//     res.send("<h1>Hello Html</h1>")
+//   }else if(req.accepts('json')){
+//     res.send({message : "Hello Json"})
+//   } else if(req.accepts('xml')){
+//     res.send({message : "Hello XML"})
+//   }else {
+//     res.send("Not Support")
+//   }
+// })
+
+
+app.get('/about', (req , res) => {
+  res.send(req.headers.host)
 })
 
 
