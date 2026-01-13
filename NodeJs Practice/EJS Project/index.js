36c87ced7c8fbb9bdc59dc49787bfcf568380865
Rizwan Ali <rizwanali4579897@gmail.com>
2026-01-13 23:54:16 +0500

    some changes

--- NodeJs Practice/EJS Project/index.js
+++ NodeJs Practice/EJS Project/index.js
@@ -49,6 +49,7 @@ app.post('/submit', (req , res) => {
     res.render("form", {message: message})
 })
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
